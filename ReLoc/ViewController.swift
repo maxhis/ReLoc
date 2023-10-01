06438bfcec8a5d15c29edc43c57a7808915d2313
iStar <istar309@gmail.com>
2023-10-01 20:40:23 +0800

    Initial Commit

---
+++ ReLoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReLoc
+//
+//  Created by iStar on 2023/10/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
